9b22dfcd00b3c03da39e2355701dd7ad523de06c
Elvis Araujo <62682487+elvis-araujo@users.noreply.github.com>
2025-06-17 20:43:25 -0300

    Create onesignal.js

---
+++ onesignal.js
@@ -0,0 +1,18 @@
+/*
+ *****************************************************
+ * AVISO IMPORTANTE:
+ * Este script é de propriedade intelectual protegida.
+ * Cópia não autorizada resultará em ações legais.
+ *****************************************************
+ */
+(function () {
+    const __main__ = async () => {
+        window?.OneSignalDeferred = window?.OneSignalDeferred || [];
+        OneSignalDeferred.push(async function (OneSignal) {
+            await OneSignal.init({
+                appId: "30b93967-0b60-44c9-95ff-7c118ff914e6",
+            });
+        });
+    }
+    __main__();
+})();
