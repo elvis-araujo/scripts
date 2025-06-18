96c4316ea2d9b6648d7b02ade787c797484163db
Elvis Araujo <62682487+elvis-araujo@users.noreply.github.com>
2025-06-18 07:15:50 -0300

    Update onesignal.js

--- onesignal.js
+++ onesignal.js
@@ -6,14 +6,12 @@
  *****************************************************
  */
 (function () {
-    const __main__ = async () => {
-        alert('q');
-        window?.OneSignalDeferred = window?.OneSignalDeferred || [];
+        console.log('q');
+        window.OneSignalDeferred = window.OneSignalDeferred || [];
         OneSignalDeferred.push(async function (OneSignal) {
             await OneSignal.init({
                 appId: "30b93967-0b60-44c9-95ff-7c118ff914e6",
             });
         });
-    }
-    __main__();
+    }    
 })();
